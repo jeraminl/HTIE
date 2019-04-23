154415adb6b693bbba02cc211a43d8936ec95d3c
jeraminl <jeramin.leong@gmail.com>
2019-04-23 10:12:53 -0700

    first init

---
+++ HTIE/main.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+from pathlib import Path
+
+
+'''
+    path = path to the folder containing the files
+    x = width
+    y = height
+    
+'''
+def crop(path, x, y, offsetX=0, offsetY=0):
+   osPath = Path(p)
+
+
+
+
+
+
+
+if __name__=="__main__":
+    p = os.getcwd()
+    path = os.listdir("E:\Users\MrSkeliman\Videos\Captures\The Elder Scrolls V  Skyrim Special Edition")
+    print(Path(path))
