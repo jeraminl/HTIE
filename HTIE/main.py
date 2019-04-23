98cf2ee6bc451df19f4d3a4a4b7037aad22212c4
jeraminl <32972952+jeraminl@users.noreply.github.com>
2019-04-23 14:46:22 -0700

    simple path system created

--- HTIE/main.py
+++ HTIE/main.py
@@ -2,23 +2,29 @@ import subprocess
 import os
 from pathlib import Path
 
-
+inputPath = Path("../input")
+outputPath = Path("../output")
 '''
     path = path to the folder containing the files
     x = width
     y = height
-    
+
 '''
-def crop(path, x, y, offsetX=0, offsetY=0):
-   osPath = Path(p)
 
 
+def crop(x, y, offsetX=0, offsetY=0):
+   for filename in os.listdir(inputPath):
+       if filename.endswith(".jpg"):
+           fullInputPath = os.path.join(inputPath, filename)
+           fullOutputPath = os.path.join(outputPath, filename)
+           command = ["convert", str(fullInputPath), "-crop", str(x)+'x'+str(y)+'+'+str(offsetX)+'+'+str(offsetY), str(fullOutputPath)]
+           subprocess.check_output(command)
 
+def rotate():
+    pass
 
 
 
 
 if __name__=="__main__":
-    p = os.getcwd()
-    path = os.listdir("E:\Users\MrSkeliman\Videos\Captures\The Elder Scrolls V  Skyrim Special Edition")
-    print(Path(path))
+    crop(120,120,960,540)
